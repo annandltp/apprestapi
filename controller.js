2f911fdb24a3ba8934447ce1ce428019fe82972c
DESKTOP-A9J1SJQ\Anandela <anan.dela211@gmail.com>
2021-03-24 22:25:46 +0700

    menampilkan data mahasiswa

--- controller.js
+++ controller.js
@@ -3,6 +3,17 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function(req,res){
-    response.ok('Aplikasi REST API ku berjalan!',res);
+exports.index = function (req, res) {
+    response.ok('Aplikasi REST API ku berjalan!', res);
+};
+
+// menampilkan semua data mahasiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, field) {
+        if (error) {
+            connection.log(error);
+        } else {
+            response.ok(rows, res);
+        }
+    });
 };
