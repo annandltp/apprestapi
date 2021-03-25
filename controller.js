73584852f2a405d7b7c1d70a66b3bfe088c8db3b
DESKTOP-A9J1SJQ\Anandela <anan.dela211@gmail.com>
2021-03-25 07:15:58 +0700

    tambah mahasiswa

--- controller.js
+++ controller.js
@@ -9,24 +9,42 @@ exports.index = function (req, res) {
 
 // menampilkan semua data mahasiswa
 exports.tampilsemuamahasiswa = function (req, res) {
-    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
-        if (error) {
-            console.log(error);
-        } else {
-            response.ok(rows, res);
-        }
-    });
+    connection.query('SELECT * FROM mahasiswa',
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error);
+            } else {
+                response.ok(rows, res);
+            }
+        });
 };
 
 // menampilkan semua data mahasiswa berdasarkan id
 exports.tampilberdasarkanid = function (req, res) {
     let id = req.params.id;
     connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id],
-        function (error, rows, fields){
-            if(error){
+        function (error, rows, fields) {
+            if (error) {
                 console.log(error);
-            }else{
+            } else {
                 response.ok(rows, res);
             }
-    });
+        });
+};
+
+// menambahkan data mahasiswa
+exports.tambahMahasiswa = function (req, res) {
+    var nim = req.body.nim;
+    var nama = req.body.nama;
+    var jurusan = req.body.jurusan;
+
+    connection.query('INSERT INTO mahasiswa (nim,nama,jurusan) VALUES (?,?,?)',
+        [nim, nama, jurusan],
+        function (error, rows, fields) {
+            if (error) {
+                console.log(error)
+            } else {
+                response.ok("Berhasil Menambahkan Data!", res)
+            }
+        });
 };
