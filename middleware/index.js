c6d0680b24690cf8894cc29d156465b110152a3e
DESKTOP-A9J1SJQ\Anandela <anan.dela211@gmail.com>
2021-03-27 21:33:18 +0700

    push user

---
+++ middleware/index.js
@@ -0,0 +1,9 @@
+// daftarkan controller auth.js
+var express = require('express');
+var auth = require('./auth');
+var router = express.Router();
+
+// daftarkan menu registrasi
+router.post('/api/v1/register', auth.registrasi);
+
+module.exports = router;
