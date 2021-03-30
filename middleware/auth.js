2771a682f045cc6d463fbef27723acc59eba1191
DESKTOP-A9J1SJQ\Anandela <anan.dela211@gmail.com>
2021-03-31 06:58:55 +0700

    login jwt

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ var ip = require('ip');
 var jwt = require('jsonwebtoken');
 
 // controller untuk register
-exports.registrasi = function(req, res) {
+exports.registrasi = function (req, res) {
     var post = {
         username: req.body.username,
         email: req.body.email,
@@ -16,29 +16,89 @@ exports.registrasi = function(req, res) {
         tanggal_daftar: new Date(),
     }
 
-    var query = "SELECT email from ?? WHERE ??";
+    var query = "SELECT email from ?? WHERE ??=?";
     var table = ["user", "email", post.email];
 
     query = mysql.format(query, table);
 
     connection.query(query, function (error, rows) {
-            if (error) {
-                console.log(error);
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 0) {
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["user"];
+                query = mysql.format(query, table);
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        response.ok("Behasi menambahkan data user baru", res);
+                    }
+                });
             } else {
-                if (rows.length == 0) {
-                    var query = "INSERT INTO ?? SET ?";
-                    var table = ["user"];
-                    query = mysql.format(query, table);
-                    connection.query(query, post, function (error, rows) {
-                        if (error) {
-                            console.log(error);
-                        } else {
-                            response.ok("Behasi menambahkan data user baru", res);
-                        }
-                    });
-                } else {
-                    response.ok("Email sudah terdaftar!");
-                }
+                response.ok("Email sudah terdaftar!", res);
             }
-        });
+        }
+    });
+}
+
+// controller untuk login
+exports.login = function (req, res) {
+    var post = {
+        password: req.body.password,
+        email: req.body.email
+    }
+
+    var query = "SELECT * FROM ?? WHERE ??=? AND ??=?";
+    var table = ["user", "password", md5(post.password), "email", post.email];
+
+    query = mysql.format(query, table);
+
+    connection.query(query, function (error, rows) {
+        if (error) {
+            console.log(error);
+        } else {
+            if (rows.length == 1) {
+                // buat token
+                var token = jwt.sign({
+                    rows
+                }, config.secret, {
+                    //ubah expires dalam ms
+                    expiresIn: '2400000'
+                });
+                id_user = rows[0].id;
+
+                // tampung data user
+                var data = {
+                    id_user: id_user,
+                    access_token: token,
+                    // cek ip address lokal
+                    ip_address: ip.address()
+                }
+
+                var query = "INSERT INTO ?? SET ?";
+                var table = ["akses_token"];
+
+                query = mysql.format(query, table);
+                connection.query(query, data, function (error, rows) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        res.json({
+                            success: true,
+                            message: 'Token JWT success generate',
+                            token: token,
+                            currUser: data.id_user
+                        });
+                    }
+                });
+            } else {
+                res.json({
+                    "Error": true,
+                    "Message": "Email atau password salah!"
+                });
+            }
+        }
+    });
 }
